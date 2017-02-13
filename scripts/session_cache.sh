b7fba54b32fa8c4b3be4d2d741a9d7d409c4bb32
Antoni F. Setiawan <antoni.fs@gmail.com>
2017-02-13 17:44:43 +0700

    Update session_cache.sh

--- scripts/session_cache.sh
+++ scripts/session_cache.sh
@@ -5,5 +5,6 @@ modman clone git://github.com/colinmollenhour/Cm_RedisSession.git
 modman clone https://github.com/GordonLesti/Lesti_Fpc.git
 
 mv /var/www/html/magento/app/etc/fpc.xml.sample /var/www/html/magento/app/etc/fpc.xml
-
 sed -i "s#<port>6379</port>#<port>6381</port>#" /var/www/html/magento/app/etc/fpc.xml
+
+modman clone https://github.com/steverobbins/Magento-Redismanager.git
